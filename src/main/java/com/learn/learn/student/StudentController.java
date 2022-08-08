b02d196f26c391c8f465bab783df5f09e9d11b22
LuanBraga <luanbraga.lb@gmail.com>
2022-08-08 16:18:29 -0300

    implements SseEmitter

--- src/main/java/com/learn/learn/student/StudentController.java
+++ src/main/java/com/learn/learn/student/StudentController.java
@@ -2,8 +2,11 @@ package com.learn.learn.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.time.LocalTime;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -38,5 +41,25 @@ public class StudentController {
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
+    }
+
+    @GetMapping("/sse-emitter")
+    public SseEmitter sseEmitter() {
+        SseEmitter emitter = new SseEmitter();
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                for (int i = 0; true; i++) {
+                    SseEmitter.SseEventBuilder event = SseEmitter.event()
+                            .id(String.valueOf(i))
+                            .name("SSE_EMITTER_EVENT")
+                            .data("SSE EMITTER - " + LocalTime.now().toString());
+                    emitter.send(event);
+                    Thread.sleep(1000);
+                }
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        });
+        return emitter;
     }
 }
